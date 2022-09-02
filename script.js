8a1c81f719c79dc96a95bb16fba965141e1cd4de
Sameer Singh <sameersingh1234pok@gmail.com>
2022-09-02 23:32:31 +0530

    Almost Completed

--- script.js
+++ script.js
@@ -1,21 +1,65 @@
-const body = document.getElementById('body');
-const btnbg = document.querySelector('.btn-bg');
-const togglebtn = document.querySelector('.toggle-btn');
+let darkmode = localStorage.getItem('darkMode');   //Establishing the Local Storage 
+const btnbg = document.querySelector('.btn-bg');   //Toggle Button Background 
+const togglebtn = document.querySelector('.toggle-btn');    //Toggle button 
 
+// Method 1
 
-togglebtn.addEventListener( 'click', ()=> {
-    if(body.classList.contains('darkMode')) {
-        body.classList.remove('darkMode');
+// Creating the Variable for Toggling the Dark Mode. 
+const EnableDarkMode = ()=> {
+    document.body.classList.add('darkMode');
+    localStorage.setItem('darkMode', 'enabled');
+    togglebtn.classList.add('shift-btn');
+}
+const DisableDarkMode = ()=> {
+    document.body.classList.remove('darkMode');
+    localStorage.setItem('darkMode', 'disabled');
+    togglebtn.classList.remove('shift-btn');
+}
+
+// To make sure if dark mode is enabled then stay enabled even after a referesh.
+
+// Checking if the value of darkmode is enabled or not.
+// If it is enabled, then stay enabled even after refereshing the page.
+// If it is disabled, then stay disabled even after refereshing the page.
+if(darkmode === 'enabled') {
+    EnableDarkMode();
+}
+
+// Working of the below If and else Statement. 
+
+// Checking if the DarkMode is enabled or not.
+// If it is Disabled, then add the darkmode class in the body. 
+// If it is Enabled, then remove the darkmode class from the body. 
+
+// Toggling The Dark Mode. 
+togglebtn.addEventListener('click', () => {
+    darkmode = localStorage.getItem('darkMode');
+    if(darkmode !== 'enabled') {
+        EnableDarkMode();
     }
     else {
-        body.classList.add('darkMode');
+        DisableDarkMode();
     }
-
-    if (togglebtn.classList.contains('shift-btn')) {
-        togglebtn.classList.remove('shift-btn');
-    }
-    else{
-        togglebtn.classList.add('shift-btn');
-    }
-    console.log("clicked");
 });
+
+// Method 2 for Toggling the Dark Mode but without Local Storage.
+
+// togglebtn.addEventListener( 'click', ()=> {
+    // For Toggling the Dark Mode. 
+    // if(document.body.classList.contains('darkMode')) {
+    //     document.body.classList.remove('darkMode');
+    // }
+    // else {
+    //     document.body.classList.add('darkMode');
+    // }
+
+        // For toggling the class position of the button.
+//     if (togglebtn.classList.contains('shift-btn')) {
+//         togglebtn.classList.remove('shift-btn');
+//     }
+//     else{
+//         togglebtn.classList.add('shift-btn');
+//     }
+// });
+
+
